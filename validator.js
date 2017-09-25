071ccd2634570d54f509ff519e644504eeb29a4e
Kate <kate.ramge@gmail.com>
2017-09-25 11:26:50 -0500

    Exercise One is completed.

--- validator.js
+++ validator.js
@@ -1,10 +1,13 @@
-function validator(pass1,pass2) {
-	if((pass1!=pass2)||(pass1.length<8)){
-		if(pass1!=pass2){
-			alert("Passwords don't match!");
-		}
-		else{
-			alert("Password too short!");
-		}
+function validator() {
+	var pass1 = document.getElementById("p1").value;
+	var pass2 = document.getElementById("p2").value;
+	if(pass1.length<8) {
+		alert("Password too short!");
+	}
+	else if(pass1==pass2) {
+		alert("Your password is ready to go");
+	}
+	else {
+		alert("Your passwords do not match!");
 	}
 }
